d70e8dd6e130626a360f31fb8ff668b42b2fa087
Carlos Gabriel <140734200+Inojoza28@users.noreply.github.com>
2024-01-07 10:50:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function respostaSim() {
+    // Mover o botão "Não" para fora da tela
+    const naoBtn = document.getElementById("naoBtn");
+    naoBtn.style.position = "absolute";
+    naoBtn.style.left = "-9999px"; // Move o botão para uma posição fora da tela
+
+    // Atualiza a mensagem
+    var mensagemElement = document.getElementById("mensagem");
+    mensagemElement.innerText = "Oxii então fecho, fala comigo no whats! Tmj, amo tu ♡";
+
+    // Adiciona estilos ao elemento de texto
+    mensagemElement.style.color = "white";
+    mensagemElement.style.fontSize = "1.2em"; // Ajuste o tamanho conforme necessário
+}
+
+
+function respostaNao() {
+    // Mover o botão "Não" para um local aleatório
+    const naoBtn = document.getElementById("naoBtn");
+    naoBtn.style.position = "absolute";
+    naoBtn.style.left = Math.random() * (window.innerWidth - naoBtn.clientWidth) + "px";
+    naoBtn.style.top = Math.random() * (window.innerHeight - naoBtn.clientHeight) + "px";
+}
