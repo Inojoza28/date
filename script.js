593b6ecdae0f9f4787f3e417c61abcf0ee8695a7
Carlos Gabriel <140734200+Inojoza28@users.noreply.github.com>
2024-04-28 23:22:09 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -11,6 +11,11 @@ function respostaSim() {
     // Adiciona estilos ao elemento de texto
     mensagemElement.style.color = "white";
     mensagemElement.style.fontSize = "1.2em"; // Ajuste o tamanho conforme necessário
+
+    // Redireciona para o vídeo do YouTube após 4 segundos
+    setTimeout(function() {
+        window.location.href = "https://www.youtube.com/watch?v=izGwDsrQ1eQ";
+    }, 3000); // 4000 milissegundos = 4 segundos
 }
 
 function mudarPosicaoNao() {
